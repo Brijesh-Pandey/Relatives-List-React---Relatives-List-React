fe0fa8c106e4056fb4608a784d869d3453d19420
Brijesh Pandey <30575594+Brijesh-Pandey@users.noreply.github.com>
2020-10-27 19:19:40 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-           <>
-//write your code here
-	</>
-        )
-    }
+  render() {
+    let list = ["brijesh", "priyanka", "soni"];
+    return (
+      <ol key="relativeList">
+        {" "}
+        {list.map((relative, index) => (
+          <li key={`relativeListItem${index + 1}`}>{relative}</li>
+        ))}
+      </ol>
+    );
+  }
 }
-
 
 export default App;
